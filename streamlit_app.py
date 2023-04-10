fc07bb6ea4375c67d6deb831c9d0bd123405c672
rmerren-snwflktrn <130387904+rmerren-snwflktrn@users.noreply.github.com>
2023-04-10 15:29:44 -0500

    Update streamlit_app.py
    Added faulty insert code

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,4 @@ streamlit.dataframe(my_data_rows)
 # allow user to add a fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+cur.execute("insert into fruit_load_list values ('from streamlit')")
